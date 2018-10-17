87855b6d0adba72466baa918900f2ce47cf2feca
zhoulu0907 <zhoulu0907@163.com>
2018-10-17 18:10:24 +0800

    springcloud.eureka-server

---
+++ my.springcloud.eureka-server/src/main/java/my/springcloud/eurekaserver/Bootstrapper.java
@@ -0,0 +1,16 @@
+package my.springcloud.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class Bootstrapper {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(Bootstrapper.class, args);
+	}
+
+}
